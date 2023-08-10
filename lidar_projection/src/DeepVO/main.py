95640fbfe50e5dcf60e8fc5d6f951d582b5f06a3
soup1997 <soup1997@naver.com>
2023-08-10 18:32:51 +0900

    2023-08-10

---
+++ lidar_projection/src/DeepVO/main.py
@@ -0,0 +1,128 @@
+import torch
+import torch.optim as optim
+import torch.nn as nn
+from torch.utils.tensorboard import SummaryWriter
+from tqdm import tqdm
+from torch.optim.lr_scheduler import StepLR
+from model import DeepVO, Criterion
+from utils.dataloader import *
+
+# define model hyperparmeters
+hyperparams = {'epochs': 100,
+               'lr': 1e-5,
+               'batch_size': 8,
+               'weight_decay': 5e-6,
+               'lr_step': 50,
+               'lr_decay_factor':0.1}
+
+def calculate_rmse(pred, gt):
+    r_mse = nn.MSELoss()(pred[:, :, :3], gt[:, :, :3])
+    p_mse = nn.MSELoss()(pred[:, :, 3:], gt[:, :, 3:])
+    r_rmse, p_rmse = torch.sqrt(r_mse), torch.sqrt(p_mse)
+
+    return r_rmse, p_rmse
+
+def valid_one_epoch(valid_loader):
+    model.eval()
+    valid_loss = 0.0
+    position_error = 0.0
+    rotation_error = 0.0
+
+    progress_bar = tqdm(valid_loader, total=len(valid_loader), desc=f'Epoch {epoch}/{num_epochs}, Valid Loss: 0.0000')
+    
+    with torch.no_grad():
+        for batch_idx, (img, gt) in enumerate(progress_bar):
+            img, gt = img.to(device), gt.to(device)
+            output = model(img)
+            loss = criterion(output, gt)
+            valid_r_error, valid_p_error = calculate_rmse(output, gt)
+
+            valid_loss += loss.item()
+            rotation_error += valid_r_error.item()
+            position_error += valid_p_error.item()
+
+            progress_bar.set_description(f'Epoch {epoch}/{num_epochs}, Valid Loss: {valid_loss / (batch_idx + 1):.4f}, Valid position error: {valid_p_error:.4f}, Valid rotation error: {valid_r_error:.4f}')
+
+    valid_loss /= len(valid_loader)
+    position_error /= len(valid_loader)
+    rotation_error /= len(valid_loader)
+    progress_bar.close()
+
+    return valid_loss, position_error, rotation_error
+
+def train_one_epoch(epoch, train_loader):
+    train_loss = 0.0
+    position_error = 0.0
+    rotation_error = 0.0
+
+    model.train()
+    progress_bar = tqdm(train_loader, total=len(train_loader), desc=f'Epoch {epoch}/{num_epochs}, Train Loss: 0.0000')
+
+    for batch_idx, (img, gt) in enumerate(progress_bar):
+        img, gt = img.to(device), gt.to(device)
+        output = model(img)  # output is (roll, pitch, yaw, x, y, z)
+        loss = criterion(output, gt)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        train_r_error, train_p_error = calculate_rmse(output, gt)
+
+        train_loss += loss.item()
+        rotation_error += train_r_error.item()
+        position_error += train_p_error.item()
+
+        progress_bar.set_description(f'Epoch {epoch}/{num_epochs}, Train Loss: {train_loss / (batch_idx + 1):.4f}, Train position error: {train_p_error:.4f}, Train rotation error: {train_r_error:.4f}')
+
+    train_loss /= len(train_loader)
+    position_error /= len(train_loader)
+    rotation_error /= len(train_loader)
+    progress_bar.close()
+
+    return train_loss, position_error, rotation_error
+
+
+if __name__=='__main__':
+    dataset_dir = '/home/smeet/catkin_ws/src/LiDAR-Inertial-Odometry/lidar_projection/src/DeepVO/dataset/'
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+    # Create the model
+    model = DeepVO(batchNorm=True)
+    model.load_Flownet()
+    model = model.to(device)
+
+    criterion = Criterion(k=100.0).to(device)
+    optimizer = torch.optim.Adam(model.parameters(), lr=hyperparams['lr'], betas=(0.9, 0.999), weight_decay=hyperparams['weight_decay'])
+    scheduler = StepLR(optimizer, gamma=hyperparams['lr_decay_factor'], step_size=hyperparams['lr_step'])
+    num_epochs = hyperparams['epochs']
+    
+    writer = SummaryWriter()
+    torch.set_printoptions(sci_mode=False, precision=10)
+    torch.autograd.set_detect_anomaly(True)
+
+    for epoch in range(1, num_epochs+1):
+        train_loader, valid_loader, _ = load_dataset(dataset_dir, batch_size=hyperparams['batch_size'], shuffle=True)
+        train_loss, train_t_acc, train_q_acc = train_one_epoch(epoch, train_loader)
+
+        writer.add_scalar("Loss/Train", train_loss, epoch)
+        writer.add_scalar("Translation Acc/Train", train_t_acc, epoch)
+        writer.add_scalar("Orientation Acc/Train", train_q_acc, epoch)
+
+        valid_loss, valid_t_acc, valid_q_acc = valid_one_epoch(valid_loader)
+        writer.add_scalar("Loss/Valid", valid_loss, epoch)
+        writer.add_scalar("Translation Acc/Valid", valid_t_acc, epoch)
+        writer.add_scalar("Orientation Acc/Valid", valid_q_acc, epoch)
+
+        if epoch % hyperparams['lr_step'] == 0:
+            scheduler.step()
+            torch.save(model.state_dict(), f"Epoch:{epoch}, Loss: {valid_loss}, Acc: {valid_t_acc}{valid_q_acc}_DeepVO.pth")
+
+    # After training, save the model
+    model.to('cpu')
+    model.eval()
+
+    torch.save(model.state_dict(), "DeepVO.pth")
+    model_scripted = torch.jit.script(model)
+    model_scripted.save("DeepVO_scripted.pt")
+    writer.close()
---
+++ lidar_projection/src/DeepVO/utils/dataloader.py
@@ -0,0 +1,122 @@
+import torch
+from torch.utils.data import Dataset, DataLoader, ConcatDataset
+import torchvision.transforms as transforms
+
+import os
+import numpy as np
+from PIL import Image
+from natsort import natsorted
+
+kitti_dataset = {'00': [0, 4540],
+                 '01': [0, 1100],
+                 '02': [0, 4660],
+                 '04': [0, 270],
+                 '05': [0, 2760],
+                 '06': [0, 1100],
+                 '07': [0, 1100],
+                 '08': [1100, 5170],
+                 '09': [0, 1590],
+                 '10': [0, 1200]}
+
+# Generate the data frames per path
+def get_data_info(dataset_dir, kitti_sequence, seq_len=7, overlap=1, drop_last=True):
+    pose_dir = os.path.join(dataset_dir, 'poses')
+    image_dir = os.path.join(dataset_dir, 'images')
+
+    img_sequence, pose_sequence = [], []
+
+    # Load & sort the raw data
+    poses = np.load(os.path.join(pose_dir, f'{kitti_sequence}.npy'))  # (n_images, 6)
+    images = natsorted([f for f in os.listdir(os.path.join(image_dir, kitti_sequence)) if f.endswith('.jpg')])
+
+    n_frames = len(images)
+    start = 0
+    
+    while (start + seq_len) < n_frames:
+        x_seg = images[start:start+seq_len]
+        img_sequence.append([os.path.join(image_dir, kitti_sequence, img) for img in x_seg])
+        pose_sequence.append(poses[start:start+seq_len-1])
+        start += seq_len - overlap
+
+    if not drop_last:
+        img_sequence.append([os.path.join(image_dir, kitti_sequence, img) for img in images[start:]])
+        pose_sequence.append(poses[start:])
+
+    # Store in a dictionary
+    data = {'image_path': img_sequence, 'pose': pose_sequence}
+    return data
+
+def load_dataset(dataset_dir, batch_size=64, shuffle=True):
+    train_datasets = []
+    valid_datasets = []
+    test_datasets = []
+
+    for seq, valid_time in kitti_dataset.items():
+        dataset = ImageSequenceDataset(dataset_dir=dataset_dir, kitti_sequence=seq, seq_len=7)
+
+        if seq == '03' or seq == '05':
+            valid_datasets.append(dataset)
+
+        elif seq == '09' or seq == '10':
+            test_datasets.append(dataset)
+
+        else:
+            train_datasets.append(dataset)
+
+    train_loader = DataLoader(dataset=ConcatDataset(train_datasets),
+                              batch_size=batch_size,
+                              shuffle=shuffle)
+
+    valid_loader = DataLoader(dataset=ConcatDataset(valid_datasets),
+                             batch_size=batch_size,
+                             shuffle=shuffle)
+    
+    test_loader = DataLoader(dataset=ConcatDataset(test_datasets),
+                             batch_size=batch_size,
+                             shuffle=shuffle)
+
+    return train_loader, valid_loader, test_loader
+
+class SubtractConstant:
+    def __init__(self, constant=0.5):
+        self.constant = constant
+
+    def __call__(self, img):
+        return (img - self.constant)
+
+
+class ImageSequenceDataset(Dataset):
+    def __init__(self, dataset_dir, kitti_sequence, seq_len=7, overlap=1, drop_last=True):
+        self.data_info = get_data_info(dataset_dir, kitti_sequence, seq_len, overlap, drop_last)
+        self.image = self.data_info['image_path']
+        self.groundtruth = self.data_info['pose']
+        self.transforms = transforms.Compose([transforms.ToTensor(),
+                                              transforms.Resize((64, 1200)),
+                                              SubtractConstant(constant=0.5)])
+
+    def __len__(self):
+        return len(self.data_info['image_path'])
+
+    def __getitem__(self, index):
+        image_sequence = []
+        image_path_sequence = self.image[index]
+
+        for img_path in image_path_sequence:
+            img_as_img = Image.open(img_path)
+            img_as_tensor = self.transforms(img_as_img).unsqueeze(0) # torch.Size([1, 3, 64, 1200]), which is (1, channel, height, width)
+            image_sequence.append(img_as_tensor)
+
+        image_sequence = torch.cat(image_sequence, 0) # torch.Size([7, 3, 64, 1200]), which is (sequence length, channel, height, width)
+
+        # Prepare the ground truth pose
+        gt_sequence = self.groundtruth[index]
+        gt_sequence = torch.tensor(gt_sequence, dtype=torch.float32) # torch.Size([6, 6]), which is (sequence length, 6dof)
+
+        return image_sequence, gt_sequence
+
+if __name__=='__main__':
+    dataset_dir = '/home/smeet/catkin_ws/src/LiDAR-Inertial-Odometry/lidar_projection/src/DeepVO/dataset/'
+    train_loader, valid_loader, test_loader = load_dataset(dataset_dir, batch_size=8, shuffle=True)
+
+    for img, gt in train_loader:
+        print(img.size(), gt.size()) # torch.Size([8, 7, 3, 64, 1200]) torch.Size([8, 6, 6])
